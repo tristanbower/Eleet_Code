f3dac239009d329b190af529c03bbf767c8dfc6b
Tristan <tristanobower@gmail.com>
2022-10-08 13:42:15 -0400

    second easy submission and the more concise version following

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -1,4 +1,18 @@
 /**
+ *Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+
+Exmaple 2
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ *
+ */
+
+/**
  * @param {number} x
  * @return {boolean}
  */
@@ -18,5 +32,3 @@ var isPalindrome = function (x) {
 var isPalindrome1 = function (x) {
   return x == x.toString().split("").reverse().join("");
 };
-
-isPalindrome(10);
