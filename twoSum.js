9ecbb174c473dfb5a695766d76ef34f2a274ba9e
Tristan <tristanobower@gmail.com>
2022-10-08 12:37:22 -0400

    First leetcode problem

---
+++ twoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const previousValues = {};
+  for (let i = 0; i <= nums.length; i++) {
+    const currentNumber = nums[i];
+    const neededValue = target - currentNumber;
+    const index2 = previousValues[neededValue];
+    if (index2 != null) {
+      return [index2, i];
+    } else {
+      previousValues[currentNumber] = i;
+    }
+  }
+};
