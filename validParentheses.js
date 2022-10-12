d2a2641b58682796cc02a408daf228d7e07d61e1
Tristan <tristanobower@gmail.com>
2022-10-12 15:37:58 -0400

    fixed bug allowing algorithm to run

--- validParentheses.js
+++ validParentheses.js
@@ -2,4 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function (s) {};
+
+var isValid = function (s) {
+  let stack = [];
+  for (i = 0; i < s.length; i++) {
+    const top = stack[stack.length - 1];
+    console.log(stack);
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+      stack.push(s[i]);
+    } else if (s[i] === ")" && top === "(" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "}" && top === "{" && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === "]" && top === "[" && stack.length !== 0) {
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
+
+isValid("{[()]}");
